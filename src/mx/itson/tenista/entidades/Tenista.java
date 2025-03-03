d840301c36221f48e4e7ec9d19e12e0f5ad7a1ae
rosales <rosales@192.168.1.13>
2025-03-02 20:07:41 -0700

    Tenista

---
+++ src/mx/itson/tenista/entidades/Tenista.java
@@ -0,0 +1,76 @@
+package mx.itson.tenista.entidades;
+
+/**
+ *
+ * @author Jesus Rosales y Kevin Murgía
+ */
+public class Tenista {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Float ranking) {
+        this.ranking = ranking;
+    }
+
+    public Float getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(Float experiencia) {
+        this.experiencia = experiencia;
+    }
+    
+    public Float getVelocidad() {
+        return velocidad;
+    }
+    
+    public void setVelocidad(Float velocidad) {
+        this.velocidad = velocidad;
+    }
+    
+    public Float getFuerza() {
+        return fuerza;
+    }
+    
+    public void setFuerza(Float fuerza) {
+        this.fuerza = fuerza;
+    }
+    
+    public String getMarcaRopa() {
+        return marcaRopa;
+    }
+    
+    public void setMarcaRopa(String marcaRopa) {
+        this.marcaRopa = marcaRopa;
+    }
+    private String nombre;
+    private Float ranking;
+    private Float experiencia;
+    private Float velocidad;
+    private Float fuerza;
+    private String marcaRopa;
+    
+    public float calcularRendimiento(Float ranking, Float experiencia) {
+        float rendimiento = ranking * experiencia;
+        return rendimiento;
+    }
+    
+    public float calcularFuerzaGolpe(Float fuerza, Float experiencia) {
+        float fuerzaGolpe = fuerza * (experiencia/2);
+        return fuerzaGolpe;
+    }
+    
+    public float calcularVelocidadCarrera(Float velocidad, Float experiencia) {
+        float velocidadReal = velocidad + (experiencia * 0.1f);
+        return velocidadReal;
+    }
+}
---
+++ src/mx/itson/tenista/ui/Main.java
@@ -0,0 +1,44 @@
+package mx.itson.tenista.ui;
+import mx.itson.tenista.entidades.Tenista;
+/**
+ *
+ * @author Jesus Rosales y Kevin Murguía
+ */
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+       
+        //Pedimos al usuario que ingrese la fuerza y la velocidad base del tenista
+        System.out.println("Ingresa la fuerza base del tenista:");
+        double fuerza = sc.nextDouble();
+        
+        System.out.println("Ingresa la velocidad base del tenista:");
+        double velocidad = sc.nextDouble();
+        
+        //Generamos una instancia de la clase Tenista
+        Tenista nadal = new Tenista();
+        
+        //Asignamos los valores a los atributos de la clase Tenista
+        nadal.setNombre("Rafael Nadal");
+        nadal.setRanking(950.0f);
+        nadal.setExperiencia(20.0f);
+        nadal.setFuerza((float)fuerza);
+        nadal.setVelocidad((float)velocidad);
+        nadal.setMarcaRopa("Nike");
+        
+        //Calculamos la fuerza y la velocidad del tenista
+        float fuerzaGolpe = nadal.calcularFuerzaGolpe(nadal.getFuerza(), nadal.getExperiencia());
+        float velocidadCarrera = nadal.calcularVelocidadCarrera(nadal.getVelocidad(), nadal.getExperiencia());
+       
+        //Mostramos los resultados
+        System.out.println("El tenista " + nadal.getNombre() 
+                + " con ranking " + nadal.getRanking() 
+                + " y usando ropa " + nadal.getMarcaRopa() 
+                + " golpea con una fuerza de " + fuerzaGolpe + " newtons"
+                + " y corre a una velocidad de " + velocidadCarrera + " km/h");
+    }
+}
