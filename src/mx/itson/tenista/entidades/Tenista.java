457769178da86a94b95aa15064f774295a5044e5
rosales <rosales@192.168.1.13>
2025-03-03 21:46:30 -0700

    Tenista

--- src/mx/itson/tenista/entidades/Tenista.java
+++ src/mx/itson/tenista/entidades/Tenista.java
@@ -1,63 +1,68 @@
 package mx.itson.tenista.entidades;
-
 /**
  *
  * @author Jesus Rosales y Kevin Murguía
  */
 public class Tenista {
+    /**
+     * @return el nombre
+     */
     public String getNombre() {
         return nombre;
     }
-
+    
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public Float getExperiencia() {
-        return experiencia;
-    }
-
-    public void setExperiencia(Float experiencia) {
-        this.experiencia = experiencia;
-    }
     
+    /**
+     * @return la velocidad
+     */
     public Float getVelocidad() {
         return velocidad;
     }
     
+    /**
+     * @param velocidad the velocidad to set
+     */
     public void setVelocidad(Float velocidad) {
         this.velocidad = velocidad;
     }
     
-    public Float getFuerza() {
-        return fuerza;
-    }
-    
-    public void setFuerza(Float fuerza) {
-        this.fuerza = fuerza;
-    }
-    
+    /**
+     * @return the marcaRopa
+     */
     public String getMarcaRopa() {
         return marcaRopa;
     }
     
+    /**
+     * @param marcaRopa the marcaRopa to set
+     */
     public void setMarcaRopa(String marcaRopa) {
         this.marcaRopa = marcaRopa;
     }
+    
+    
     private String nombre;
-    private Float experiencia;
+    
     private Float velocidad;
-    private Float fuerza;
+    
     private String marcaRopa;
     
-
-    public float calcularFuerzaGolpe(Float fuerza, Float experiencia) {
-        float fuerzaGolpe = fuerza * (experiencia/2);
-        return fuerzaGolpe;
-    }
-    
-    public float calcularVelocidadCarrera(Float velocidad, Float experiencia) {
-        float velocidadReal = velocidad + (experiencia * 0.1f);
-        return velocidadReal;
+    /**
+     * Calcula la presión que ejerce el golpe sobre el área de la pelota.
+     * @param fuerzaGolpe Fuerza directa del golpe ingresada por el usuario (Newtons).
+     * @param areaPelota Área de contacto de la pelota (metros cuadrados).
+     * @return Presión ejercida sobre la pelota (Pascales).
+     */
+    public float calcularPresionGolpe(Float fuerzaGolpe, Float areaPelota) {
+        // La presión es la fuerza dividida por el área
+        float presionEjercida = fuerzaGolpe / areaPelota;
+        
+        return presionEjercida;
     }
 }
--- src/mx/itson/tenista/ui/Main.java
+++ src/mx/itson/tenista/ui/Main.java
@@ -5,38 +5,37 @@ import mx.itson.tenista.entidades.Tenista;
  * @author Jesus Rosales y Kevin Murguía
  */
 import java.util.Scanner;
-
 public class Main {
     
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
        
-        //Pedimos al usuario que ingrese la fuerza y la velocidad base del tenista
-        System.out.println("Ingresa la fuerza base del tenista:");
-        double fuerza = sc.nextDouble();
+        // Pedimos al usuario que ingrese la fuerza del golpe y el área de la pelota
+        System.out.println("Ingresa la fuerza del golpe (en Newtons):");
+        double fuerzaGolpe = sc.nextDouble();
         
-        System.out.println("Ingresa la velocidad base del tenista:");
+        System.out.println("Ingresa el área de contacto de la pelota (en metros cuadrados):");
+        double areaPelota = sc.nextDouble();
+        
+        System.out.println("Ingresa la velocidad del tenista (en km/h):");
         double velocidad = sc.nextDouble();
         
-        //Generamos una instancia de la clase Tenista
+        // Generamos una instancia de la clase Tenista
         Tenista nadal = new Tenista();
         
-        //Asignamos los valores a los atributos de la clase Tenista
+        // Asignamos los valores a los atributos de la clase Tenista
         nadal.setNombre("Rafael Nadal");
-        nadal.setExperiencia(20.0f);
-        nadal.setFuerza((float)fuerza);
         nadal.setVelocidad((float)velocidad);
         nadal.setMarcaRopa("Nike");
         
-        //Calculamos la fuerza y la velocidad del tenista
-        float fuerzaGolpe = nadal.calcularFuerzaGolpe(nadal.getFuerza(), nadal.getExperiencia());
-        float velocidadCarrera = nadal.calcularVelocidadCarrera(nadal.getVelocidad(), nadal.getExperiencia());
+        // Calculamos la presión del golpe sobre la pelota
+        float presionGolpe = nadal.calcularPresionGolpe((float)fuerzaGolpe, (float)areaPelota);
        
-        //Mostramos los resultados
+        // Mostramos los resultados
         System.out.println("El tenista " + nadal.getNombre()   
                 + " usando ropa " + nadal.getMarcaRopa() 
-                + " golpea con una fuerza de " + fuerzaGolpe + " newtons"
-                + " y corre a una velocidad de " + velocidadCarrera + " km/h");
+                + " golpea con una presión de " + presionGolpe + " pascales"
+                + " y se mueve a una velocidad de " + nadal.getVelocidad() + " km/h");
     }
 }
