c2eb6c125fee7076b0a48ae3b22aa2563fd2edfe
rosales <rosales@192.168.1.13>
2025-03-03 22:09:49 -0700

    Correcciones finales (finales)

--- src/mx/itson/tenista/entidades/Tenista.java
+++ src/mx/itson/tenista/entidades/Tenista.java
@@ -4,51 +4,60 @@ package mx.itson.tenista.entidades;
  * @author Jesus Rosales y Kevin Murguía
  */
 public class Tenista {
-/**
- * 
- * @return Nombre
- */
+    /**
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
     
-   /**
-    * 
-    * @param nombre 
-    */
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
     
     /**
-     * @return Velocidad
+     * @return the velocidad
      */
     public Float getVelocidad() {
         return velocidad;
     }
     
     /**
-     * 
-     * @param velocidad 
+     * @param velocidad the velocidad to set
      */
     public void setVelocidad(Float velocidad) {
         this.velocidad = velocidad;
     }
     
     /**
-     * 
-     * @return MarcaRopa
+     * @return the marcaRopa
      */
     public String getMarcaRopa() {
         return marcaRopa;
     }
     
-   /**
-    * 
-    * @param marcaRopa 
-    */
+    /**
+     * @param marcaRopa the marcaRopa to set
+     */
     public void setMarcaRopa(String marcaRopa) {
         this.marcaRopa = marcaRopa;
+    }
+    
+    /**
+     * @return the presion
+     */
+    public Float getPresion() {
+        return presion;
+    }
+    
+    /**
+     * @param presion the presion to set
+     */
+    public void setPresion(Float presion) {
+        this.presion = presion;
     }
     
     
@@ -58,8 +67,11 @@ public class Tenista {
     
     private String marcaRopa;
     
+    private Float presion;
+    
     /**
      * Calcula la presión que ejerce el golpe sobre el área de la pelota.
+     * También modifica el atributo presion de la clase.
      * @param fuerzaGolpe Fuerza directa del golpe ingresada por el usuario (Newtons).
      * @param areaPelota Área de contacto de la pelota (metros cuadrados).
      * @return Presión ejercida sobre la pelota (Pascales).
@@ -67,6 +79,9 @@ public class Tenista {
     public float calcularPresionGolpe(Float fuerzaGolpe, Float areaPelota) {
         // La presión es la fuerza dividida por el área
         float presionEjercida = fuerzaGolpe / areaPelota;
+        
+        // Modificamos el atributo presion de la clase
+        this.presion = presionEjercida;
         
         return presionEjercida;
     }
